54231d77898ff4360aa59d6215e6dc63e1657ac1
Gokul <iamgokul2401@gmail.com>
2022-11-30 07:04:05 +0530

    first change

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SSBAST</title>
+    <link rel="stylesheet" href="./assets/css/landingPage/style.css">
+</head>
+<body>
+    <?php
+    include('./views/authHead.php');
+    ?>
+    <main>
+        <section>
+            <div class="landingContainer">
+                <div class="logo">
+                    <h1 class="brandName" >ssbaide <sup><img src="./assets/icons/ssbast.svg" alt="logo" /></sup> </h1>
+                    <p>Hey, Welcome to new space</p>
+                    <p class="describe" >Hi, I am ssbaide AI Application, wish to help students and faculties.Main goal of my thought is to navigate students into strongest path.
+                    don't worry I'm with you, we grow together. My soul has been maintained by Sri Sankara Bhagavathi Arts and Science College. </p>
+                    <p class="authNav" >Did you engage with me already? kick <b>Sign in</b>, Otherwise going towards <b>Sign up</b></p>
+                </div>
+                <img class="sitImg" src="./assets/icons/sitimg.svg" alt="">
+                <img class="grad" src="./assets/icons/graduate.svg" alt="">
+            </div>
+        </section>
+    </main>
+</body>
+</html>
---
+++ views/authHead.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="headContent">
+        <nav>
+                <ul>
+                    <li> <a href="#"> <img src="./assets/icons/house.svg" alt="homeIcon"> <span>Home</span> </a> </li>
+                    <li> <a href="#"> <img src="./assets/icons/bell.svg" alt="notificationicon"> <span>Info</span> </a> </li>
+                    <li> <a href="#"> <img src="./assets/icons/person-rolodex.svg" alt="contactIcon"> <span>Contact</span> </a> </li>
+                    <li> <a href="#"> <img src="./assets/icons/info-square.svg" alt="infoIcon"> <span>Info</span> </a> </li>
+                </ul>
+            </nav>
+            <div class="auth">
+                <a class="button"  href="/signin">Sign in</a>
+                <a class="button-outline" style="margin-left:20px"  href="/signup">Sign up</a>
+        </div>
+    </div>        
+</header>
